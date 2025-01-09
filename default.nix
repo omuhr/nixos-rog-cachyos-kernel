d20e7f803f4c02821dcb61a77191fbf1c8cd3954
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-01-09 20:34:01 +0000

    Adjust Nixpkgs tarball URL

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
       lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
     in
     (import (fetchTarball {
-      url = "https://github.com/${lock.owner}/${lock.repo}/archives/refs/${lock.rev}.tar.gz";
+      url = "https://github.com/${lock.owner}/${lock.repo}/archive/${lock.rev}.tar.gz";
       sha256 = lock.narHash;
     }) { }),
 }:
