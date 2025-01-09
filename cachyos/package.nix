381cf29b31f82fac555d358addb11e0bf1007c30
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-01-09 20:27:01 +0000

    Revert to Linux 6.6.69 and apply BORE patches

--- cachyos/package.nix
+++ cachyos/package.nix
@@ -1,25 +1,46 @@
 {
+  lib,
   buildLinux,
   fetchzip,
-  lib,
   fetchpatch,
 }:
-buildLinux rec {
+let
+  linuxVersion = "6.6.69";
+  linuxHash = "sha256-nCla3GbDxPy56r5K1HOpyAJ+8YyE792z5a4eH5I1S7s=";
+
+  cachyosCommit = "7ed2c2912029bc74b8276b7922164d7b538caea9";
+  cachyosHash = "sha256-zOZ1wUdpjAul7qZ/YsVXnz1exKE69k9mzC81os2IBFA=";
+  boreHash = "sha256-Tz7yxrwo3kzd2J/BvX3HEQmVcMD2ILxFXvY/d46iB7I=";
+in
+buildLinux {
   src = fetchzip {
-    url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-    hash = "sha256-ASEcl8afiNYl6xDY9cZz5rdskaVy8f36d4Aart7/I14=";
+    url = "mirror://kernel/linux/kernel/v${lib.versions.major linuxVersion}.x/linux-${linuxVersion}.tar.xz";
+    hash = linuxHash;
   };
 
-  version = "6.12.9";
+  version = linuxVersion;
 
-  kernelPatches = [
-    {
-      name = "cachyos-base-all";
+  kernelPatches =
+    let
+      linuxMinorVersion = lib.versions.majorMinor linuxVersion;
+    in
+    [
+      {
+        name = "cachyos-base-all";
 
-      patch = fetchpatch {
-        url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/c8e2591aa83fdec3345f9699030926715db727d0/${lib.versions.majorMinor version}/all/0001-cachyos-base-all.patch";
-        hash = "sha256-k4WzphgNureRZEp6h4spfTIlIpdKHQT34MCwyS6B6jc=";
-      };
-    }
-  ];
+        patch = fetchpatch {
+          url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/${cachyosCommit}/${linuxMinorVersion}/all/0001-cachyos-base-all.patch";
+          hash = cachyosHash;
+        };
+      }
+
+      {
+        name = "bore-cachy";
+
+        patch = fetchpatch {
+          url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/${cachyosCommit}/${linuxMinorVersion}/sched/0001-bore-cachy.patch";
+          hash = boreHash;
+        };
+      }
+    ];
 }
