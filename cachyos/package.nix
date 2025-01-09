ff1966a2c19619f08af43c1d22d6a379a523985f
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-01-09 18:27:08 +0000

    Update to Linux 6.12.9 and apply CachyOS patches

--- cachyos/package.nix
+++ cachyos/package.nix
@@ -2,15 +2,24 @@
   buildLinux,
   fetchzip,
   lib,
+  fetchpatch,
 }:
-let
-  version = "6.6.70";
-in
-buildLinux {
+buildLinux rec {
   src = fetchzip {
     url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-    hash = "sha256-A9zR8fCbeYtBqQ+JCbN7h22DGCBjalGzKBoGXqvaiqA=";
+    hash = "sha256-ASEcl8afiNYl6xDY9cZz5rdskaVy8f36d4Aart7/I14=";
   };
 
-  inherit version;
+  version = "6.12.9";
+
+  kernelPatches = [
+    {
+      name = "cachyos-base-all";
+
+      patch = fetchpatch {
+        url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/c8e2591aa83fdec3345f9699030926715db727d0/${lib.versions.majorMinor version}/all/0001-cachyos-base-all.patch";
+        hash = "sha256-k4WzphgNureRZEp6h4spfTIlIpdKHQT34MCwyS6B6jc=";
+      };
+    }
+  ];
 }
