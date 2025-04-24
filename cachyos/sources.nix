906d68e7c93f6fbcd04c47875cbc0d38fa897929
muoscar <oscar.muhr@gmail.com>
2025-04-24 16:49:56 +0200

    hash bs

--- cachyos/sources.nix
+++ cachyos/sources.nix
@@ -8,18 +8,18 @@ let
 
   # cachyosHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
 
-  amd-pstateHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
+  amd-pstateHash = "sha256-rwDe33RDqBTwyrWT60odwXJheb0yMDG/AOAp0bT9keo=";
   amd-tlb-broadcastHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
-  asusHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
-  bbr3Hash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
+  asusHash = "sha256-vpKsaVIu89z7WlUb/yqNjp4CRtjvxjQZyho7dEJpqWA=";
+  bbr3Hash = "sha256-zVhvUS5aO/sJxJiKzRFFVH1rfD7hTCw5CMg5kVr9R64=";
   cachyHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
-  cryptoHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
+  cryptoHash = "sha256-yPfrx3HWVyLeBVa+/vZEsGfVdt5/lRWGINYfa0cUpE8=";
   fixesHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
   t2Hash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
   zstdHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
   zotac-zoneHash = "sha256-WbACCKuU9+Pc8Tnb2xVmDrdWmTTG4/x59flzWVdzrB8=";
 
-  boreHash = "sha256-uhuso01DVwuWO26z9Q9xf4TfGSmZOXkqZi4AvIjHCUY=";
+  boreHash = "sha256-qDE2IlxLkVykYX23P05Uc5JWT/1oeo3tVKhtPsCDrzM=";
 in {
   inherit linuxVersion linuxMinorVersion;
 
